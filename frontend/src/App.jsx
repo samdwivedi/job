56316660d316af5fb17d76f882a42d3bcc9daaac
Akshatt02 <akshatjaipuriar@gmail.com>
2026-02-25 17:24:06 +0530

    React config files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { Routes, Route, Link } from 'react-router-dom';
+
+export default function App() {
+
+  return (
+    <div className='p-4 bg-black text-white flex gap-4'>
+      Hi Tailwind is working.
+    </div>
+  );
+}
